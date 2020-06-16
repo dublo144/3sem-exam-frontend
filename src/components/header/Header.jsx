158d939feec868933d03e65130e7144b113eeebe
Mads Brandt <mads.brandt93@gmail.com>
2020-06-16 21:35:52 +0200

    New Build, New Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -23,31 +23,18 @@ export default function Header() {
           primary
           style={{ marginRight: '0.5em' }}
         />
-        <Dropdown text='Shopping' pointing className='link item'>
+        <Dropdown
+          text={username}
+          pointing
+          className='link item'
+          style={{ marginRight: '0.5em' }}
+        >
           <Dropdown.Menu>
             <Dropdown.Header>Categories</Dropdown.Header>
-            <Dropdown.Item>
-              <Dropdown text='Clothing'>
-                <Dropdown.Menu>
-                  <Dropdown.Header>Mens</Dropdown.Header>
-                  <Dropdown.Item>Shirts</Dropdown.Item>
-                  <Dropdown.Item>Pants</Dropdown.Item>
-                  <Dropdown.Item>Jeans</Dropdown.Item>
-                  <Dropdown.Item>Shoes</Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Header>Womens</Dropdown.Header>
-                  <Dropdown.Item>Dresses</Dropdown.Item>
-                  <Dropdown.Item>Shoes</Dropdown.Item>
-                  <Dropdown.Item>Bags</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
+            <Dropdown.Item as={NavLink} to={`/myMenuPlans`}>
+              My Menu Plans
             </Dropdown.Item>
-            <Dropdown.Item>Home Goods</Dropdown.Item>
-            <Dropdown.Item>Bedroom</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Header>Order</Dropdown.Header>
-            <Dropdown.Item>Status</Dropdown.Item>
-            <Dropdown.Item>Cancellations</Dropdown.Item>
+            <Dropdown.Item>My Shopping Lists</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
         <NewModal
